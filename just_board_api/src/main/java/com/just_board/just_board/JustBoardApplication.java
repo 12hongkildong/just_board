330d5e8f1bf9103e890896167eccf6f31c032979
“Yoo” <12hongkildong@gmail.com>
2023-07-24 17:54:49 +0900

    init(main) just_board 첫 init

---
+++ just_board_api/src/main/java/com/just_board/just_board/JustBoardApplication.java
@@ -0,0 +1,13 @@
+package com.just_board.just_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustBoardApplication.class, args);
+	}
+
+}
