da1ee417f41d729ee4fc6370e5daaf0a41182c71
YooWoo <12hongkildong@gamil.com>
2023-07-25 16:55:21 +0900

    main(feat) tailwind 설치 중 영화보러 감

---
+++ just_board_vue/src/components/login/Main.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <section>
+        <section>
+            <h1>header</h1>
+        </section>
+        <section>
+            <h1 class="text-8xl">몸통</h1>
+            <div class="text-red-500">아다다다다</div>
+        </section>
+    </section>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+    
+</style>
