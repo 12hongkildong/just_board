91b79ca5707783898526a59f47c43dbb79f55a8f
YooWoo <12hongkildong@gamil.com>
2023-07-28 16:48:31 +0900

    main(design) 게시판, 디테일, 대략적인 퍼블리싱은 완성 한 거 같음. 이제 DB만들고 ERD만들고 하면 될 거 같음

---
+++ just_board_vue/src/components/Board/Write.vue
@@ -0,0 +1,22 @@
+<template lang="">
+    <section class="grid grid-cols-[minmax(26.75rem,_1fr)_66.5rem_minmax(26.75rem,_1fr)] mt-[5.625rem]"> <!-- 가장 겉 그리드 -->
+        <section class="col-start-2">
+            <input type="text" class="text-2xl h-[4.375rem] bg-[#D9D9D9] justify-items-center content-center w-[66.5rem] pl-7 pr-7 text-2xl" placeholder="제목을 입력하세요." >
+            <section>
+                <h1 class=hidden>게시글</h1>
+                <textarea name="" id="" cols="30" rows="24" class="text-xl w-[66.5rem] border-solid border-2 border-[#35469C] p-9 resize-none" placeholder="내용을 입력하세요."></textarea>
+                <div class="grid justify-items-end">
+                    <button class="h-[4rem] w-[5rem] bg-[#35469C] text-white text-3xl rounded-xl mt-5 ">등록</button>
+                </div>
+            </section>
+        </section>
+    </section>
+
+</template>
+<script setup>
+
+
+</script>
+<style scoped>
+    
+</style>
--- just_board_vue/src/router/index.js
+++ just_board_vue/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../components/Login/LoginMain.vue'
+import Signup from '../components/Signup/MakeAccount.vue'
+
 import Board from '../components/Board/BoardMain.vue'
+  import Detail from '../components/Board/Detail.vue'
+  import Write from '../components/Board/Write.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,13 +12,35 @@ const router = createRouter({
     {
       path: '/',
       name: 'login',
-      component: Login
+      component: Login,
+      children:[
+        // {
+        //   path: 'signup',
+        //   name: 'signup',
+        //   component: Signup
+        // },
+      ]
     },
     {
       path: '/board',
       name: 'board',
-      component: Board
-    }
+      component: Board,
+    },
+    {
+      path: '/write',
+      name: 'write',
+      component: Write
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
+    },
+    {
+      path: '/1',
+      name: 'detail',
+      component: Detail
+    },
   ]
 })
 
