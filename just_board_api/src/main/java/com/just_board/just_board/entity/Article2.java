62a8cb208053bef0d103fd7c6f581eb800f5709c
YooWoo <12hongkildong@gamil.com>
2023-08-03 15:51:58 +0900

    feat(main) postgreSQL과 spring 연동 성공, 이전 실패는 오타가 있었던 거 같다. 이제 백엔드 연동 다 완료하고 더미데이터 넣은 뒤 프론트랑 연결하면 된다.

--- just_board_api/src/main/java/com/just_board/just_board/entity/Article2.java
+++ just_board_api/src/main/java/com/just_board/just_board/entity/Article2.java
@@ -1,6 +1,6 @@
 package com.just_board.just_board.entity;
 
-import java.security.Timestamp;
+import java.util.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,29 +8,28 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@Builder
 @Table(name = "article")
-public class Article {
+public class Article2 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String subject;
     private String content;
-    @Column(name="member_id")
+    @Column(name = "member_id")
     private int memberId;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Timestamp date;
+    private Date date;
     private int hit;
     private int like;
+
+
 }
---
+++ just_board_api/src/main/java/com/just_board/just_board/service/ArticleService2.java
@@ -0,0 +1,9 @@
+package com.just_board.just_board.service;
+
+import java.util.List;
+
+import com.just_board.just_board.entity.Article2;
+
+public interface ArticleService2 {
+    List<Article2> getByListAll();
+}
